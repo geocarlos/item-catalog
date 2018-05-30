d4cf1a18522dd168f623561d1f2c66457df9ff29
Geocarlos <geocarlos.lopesalves@gmail.com>
2018-05-30 10:07:22 +0000

    Create database setup

--- database_setup.py
+++ database_setup.py
@@ -10,5 +10,17 @@ Base = declarative_base()
 
 class Category(Base):
     __tablename__ = 'category'
-    name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    description = Column(String(500), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
