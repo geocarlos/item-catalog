c43c8e693c11073d1e6d5ea4fa998750ca0ca88c
Geocarlos <geocarlos.lopesalves@gmail.com>
2018-06-03 23:33:59 +0000

    Add Google and Facebook oauth service

--- database_setup.py
+++ database_setup.py
@@ -9,6 +9,16 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
@@ -22,6 +32,7 @@ class Category(Base):
             'name': self.name
         }
 
+
 class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
@@ -30,7 +41,6 @@ class Item(Base):
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
     time_added = Column(DateTime(timezone=True), server_default=func.now())
-
 
     @property
     def serialize(self):
@@ -42,6 +52,7 @@ class Item(Base):
             'description': self.description,
             'time_added': self.time_added
         }
+
 
 engine = create_engine('sqlite:///catalog.db')
 
